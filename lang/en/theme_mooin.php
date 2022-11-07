8e668aab0fe815dca6522fd5465debbe4fabddfe
Kreuterberg <mails@jonasreuter.com>
2022-11-07 13:59:18 +0100

    codechecker fixes

--- lang/en/theme_mooin.php
+++ lang/en/theme_mooin.php
@@ -23,38 +23,38 @@
  */
 
 
-// This line protects the file from being accessed by a URL directly.                                                               
+// This line protects the file from being accessed by a URL directly.
 defined('MOODLE_INTERNAL') || die();
-// The name of the second tab in the theme settings.                                                                                
-$string['advancedsettings'] = 'Advanced settings';                                                                                  
-// The brand colour setting.                                                                                                        
-$string['brandcolor'] = 'Brand colour';                                                                                             
-// The brand colour setting description.                                                                                            
-$string['brandcolor_desc'] = 'The accent colour.';     
-// A description shown in the admin theme selector.                                                                                 
-$string['choosereadme'] = 'Theme mooin is a child theme of Boost.';                
-// Name of the settings pages.                                                                                                      
-$string['configtitle'] = 'mooin settings';                                                                                          
-// Name of the first settings tab.                                                                                                  
-$string['generalsettings'] = 'General settings';                                                                                    
-// The name of our plugin.                                                                                                          
-$string['pluginname'] = 'mooin';                                                                                                    
-// Preset files setting.                                                                                                            
-$string['presetfiles'] = 'Additional theme preset files';                                                                           
-// Preset files help text.                                                                                                          
+// The name of the second tab in the theme settings.
+$string['advancedsettings'] = 'Advanced settings';
+// The brand colour setting.
+$string['brandcolor'] = 'Brand colour';
+// The brand colour setting description.
+$string['brandcolor_desc'] = 'The accent colour.';
+// A description shown in the admin theme selector.
+$string['choosereadme'] = 'Theme mooin is a child theme of Boost.';
+// Name of the settings pages.
+$string['configtitle'] = 'mooin settings';
+// Name of the first settings tab.
+$string['generalsettings'] = 'General settings';
+// The name of our plugin.
+$string['pluginname'] = 'mooin';
+// Preset files setting.
+$string['presetfiles'] = 'Additional theme preset files';
+// Preset files help text.
 $string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> for information on creating and sharing your own preset files, and see the <a href=http://moodle.net/boost>Presets repository</a> for presets that others have shared.';
-// Preset setting.                                                                                                                  
-$string['preset'] = 'Theme preset';                                                                                                 
-// Preset help text.                                                                                                                
-$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';                                                  
-// Raw SCSS setting.                                                                                                                
-$string['rawscss'] = 'Raw SCSS';                                                                                                    
-// Raw SCSS setting help text.                                                                                                      
-$string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';       
-// Raw initial SCSS setting.                                                                                                        
-$string['rawscsspre'] = 'Raw initial SCSS';                                                                                         
-// Raw initial SCSS setting help text.                                                                                              
+// Preset setting.
+$string['preset'] = 'Theme preset';
+// Preset help text.
+$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
+// Raw SCSS setting.
+$string['rawscss'] = 'Raw SCSS';
+// Raw SCSS setting help text.
+$string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';
+// Raw initial SCSS setting.
+$string['rawscsspre'] = 'Raw initial SCSS';
+// Raw initial SCSS setting help text.
 $string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
-// We need to include a lang string for each block region.                                                                          
+// We need to include a lang string for each block region.
 $string['region-side-pre'] = 'Right';
 
