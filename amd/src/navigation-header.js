9ef984831342348f548ccba4a7c472905e673d93
Kreuterberg <mails@jonasreuter.com>
2023-02-16 14:39:14 +0100

    translate the navheader on scroll

---
+++ amd/src/navigation-header.js
@@ -0,0 +1,33 @@
+/**
+ * Scrollheader function
+ */
+export function scrollHeader() {
+  //const body = document.body;
+  const scrollUp = "scroll-up";
+  const scrollDown = "scroll-down";
+  let lastScroll = 0;
+  const navigationHeader = document.getElementById("custom-top-nav");
+  //const innerNav = document.querySelector(".inner-title-navigation");
+
+  window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= 0) {
+      navigationHeader.classList.remove(scrollUp);
+      return;
+    }
+
+    if (currentScroll > lastScroll && !navigationHeader.classList.contains(scrollDown)) {
+      // down
+      navigationHeader.classList.remove(scrollUp);
+      navigationHeader.classList.add(scrollDown);
+    } else if (
+      currentScroll < lastScroll &&
+      navigationHeader.classList.contains(scrollDown)
+    ) {
+      // up
+      navigationHeader.classList.remove(scrollDown);
+      navigationHeader.classList.add(scrollUp);
+    }
+    lastScroll = currentScroll;
+  });
+}
