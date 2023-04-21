fbf6283ca3504cf2ef9fcc5f3484e845f60e9be7
Kreuterberg <mails@jonasreuter.com>
2023-04-21 11:40:54 +0200

    header height now depends on sectionname height

--- amd/src/navigation-header.js
+++ amd/src/navigation-header.js
@@ -7,11 +7,16 @@ export function scrollHeader() {
   const scrollDown = "scroll-down";
   let lastScroll = 0;
   const navigationHeader = document.getElementById("custom-top-nav");
+  const title = document.getElementById("sectionname-container");
+  //const titleHeight = title.offsetHeight + 20;
+  const breadcrumbContainer = document.getElementById("mooin-breadcrumb-container");
+
   //const navigationHeader = document.getElementById("region-main-box");
   //const navigationHeader = document.querySelector(".single-section");
 
   window.addEventListener("scroll", () => {
     const currentScroll = window.pageYOffset;
+    var titleHeight = title.offsetHeight + 20;
     if (currentScroll <= 0) {
       navigationHeader.classList.remove(scrollUp);
       return;
@@ -21,6 +26,9 @@ export function scrollHeader() {
       // down
       navigationHeader.classList.remove(scrollUp);
       navigationHeader.classList.add(scrollDown);
+      navigationHeader.style.transform = "translateY(-" + titleHeight + "px)";
+      breadcrumbContainer.style.transform = "translateY(" + titleHeight + "px)";
+      title.style.transform = "translateY(" + titleHeight + "px)";
     } else if (
       currentScroll < lastScroll &&
       navigationHeader.classList.contains(scrollDown)
@@ -28,6 +36,9 @@ export function scrollHeader() {
       // up
       navigationHeader.classList.remove(scrollDown);
       navigationHeader.classList.add(scrollUp);
+      navigationHeader.style.transform = "translateY(0px)";
+      breadcrumbContainer.style.transform = "translateY(0px)";
+      title.style.transform = "translateY(0px)";
     }
     lastScroll = currentScroll;
   });
