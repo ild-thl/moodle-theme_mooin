c104f4cad1aa40db2f591e018daba2b9e8863f9c
tinjohn <tina.john@th-luebeck.de>
2024-10-22 09:09:53 +0200

    version update and readable version

--- version.php
+++ version.php
@@ -4,11 +4,12 @@
 // This line protects the file from being accessed by a URL directly.                                                               
 defined('MOODLE_INTERNAL') || die();                                                                                                
                                                                                                                                     
-// This is the version of the plugin.                                                                                               
-$plugin->version = '2024091801';                                                                                                    
-                                                                                                                                    
+
+$plugin->version   = 2024102201;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 'v4.4.'; 
+
 // This is the version of Moodle this plugin requires.                                                                              
-$plugin->requires = '2022112805';                                                                                                   
+$plugin->requires = 2022112805;                                                                                                   
                                                                                                                                     
 // This is the component name of the plugin - it always starts with 'theme_'                                                        
 // for themes and should be the same as the name of the folder.                                                                     
